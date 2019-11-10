66600c0c12bcc05ee3a49cb12924553481550de0
Michael Cheng <michael_cheng@tech.gov.sg>
2019-11-10 17:24:54 +0800

    Resets to nothing.

--- play.rb
+++ play.rb
@@ -1,21 +1,7 @@
 require_relative './game_of_life'
 require 'tty-table'
 
-def parse_seed(input)
-  seed = []
-  input.each_index do |row_index|
-    col_cells = input[row_index].split('')
-    col_cells.each_index do |col_index|
-      seed << [col_index, row_index] if col_cells[col_index] == 'O'
-    end
-  end
-
-  seed
-end
-
-def render_table(life)
-  grid_size = life.max_grid_size
-
+def render_table(occupied_cells = [], grid_size = 5)
   table_rows = []
   grid_size.times.each do |row|
     table_rows[row] = []
@@ -24,7 +10,7 @@ def render_table(life)
     end
   end
 
-  life.board.each do |cell|
+  occupied_cells.each do |cell|
     x, y = cell
 
     table_rows[y][x] = 'O'
@@ -34,67 +20,16 @@ def render_table(life)
   puts table.render(:unicode)
 end
 
-# [ x, y ]
-# [ col, row ]
+TOTAL_EVOLUTIONS = 5
 
-# Still life
-# Block
-# seed = [
-#   [1, 1], [2, 1], [1, 2], [2, 2]
-# ]
-# input = [
-#   '.....',
-#   '.OO..',
-#   '.OO..'
-# ]
-
-# Tub
-# seed = [
-#   [2, 1], [1, 2], [3, 2], [2, 3]
-# ]
-# input = [
-#   '.....',
-#   '..O..',
-#   '.O.O.',
-#   '..O..'
-# ]
-
-# Oscillators
-# Blinker
-# seed = [
-#   [2, 1], [2, 2], [2, 3]
-# ]
-# input = [
-#   '.....',
-#   '..O..',
-#   '..O..',
-#   '..O..',
-#   '.....'
-# ]
-
-# Spaceships
-# Glider
-# seed = [
-#   [2, 1], [3, 2], [1, 3], [2, 3], [3, 3]
-# ]
-
-input = [
-  '.....',
-  '.O...',
-  '..OO.',
-  '.OO.O',
-  '..O..'
-]
-
-seed = parse_seed(input)
-life = GameOfLife.new(seed)
-
-20.times.each do |generation|
+TOTAL_EVOLUTIONS.times.each do |generation|
   puts "\e[H\e[2J"
 
   puts "Generation #{generation + 1}"
-  render_table(life)
+
+  # Gets occupied cells & grid size to render
+  render_table
   sleep 1
 
-  life.tick
+  # Tick to next generation
 end
